9e19c9beb26068fea7bcb2d3e10f54753a6c4479
Leandro Freire <leandro.freire.work@gmail.com>
2019-09-25 23:04:09 -0300

    update angular

--- src/app/pages/contato/contato.component.ts
+++ src/app/pages/contato/contato.component.ts
@@ -9,26 +9,23 @@ import { trigger, transition, style, animate, state, group } from '@angular/anim
     // animation triggers go here
     trigger('flyInOut', [
       state('in', style({
-        width: 120,
         transform: 'translateX(0)', opacity: 1
       })),
-      transition('void => *', [
+      transition(':enter', [
         style({ width: 10, transform: 'translateX(50px)', opacity: 0 }),
         group([
           animate('0.3s 0.1s ease', style({
             transform: 'translateX(0)',
-            width: 120
           })),
           animate('0.3s ease', style({
             opacity: 1
           }))
         ])
       ]),
-      transition('* => void', [
+      transition(':leave', [
         group([
           animate('0.3s ease', style({
             transform: 'translateX(50px)',
-            width: 10
           })),
           animate('0.3s 0.2s ease', style({
             opacity: 0
